332185e49ff7b9436c7ab5d8697b41199637a172
WandaSimeoli <wandasimeoli@gmail.com>
2023-09-01 21:38:59 +0200

    Bonus 1

--- Movie.php
+++ Movie.php
@@ -5,15 +5,21 @@ class Movie {
     public $year;
     public $genre;
 
-    public function __construct($title, $runningTime, $year, $genre)
+    public function __construct($title, $runningTime, $year, $myArray = [])
     {
        $this->title = $title;
        $this->runningTime = $runningTime;
        $this->year = $year;
-       $this->genre = $genre;
+       $this->genre = $myArray;
        
     }
     public function getMovie() {
-        return $this->title.' '.$this->year.' '.$this->runningTime.' '.$this->genre;
+        return $this->title.' '.$this->year.' '.$this->runningTime;
+    }
+    public function getArray() {
+        return $this->genre;
+    }
+    public function setArray($myArray) {
+        $this->genre = $myArray;
     }
 }
--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
 require __DIR__.'/Movie.php';
 
-$americanBeauty = new Movie('American Beauty', '2h','1999', 'drama');
+$americanBeauty = new Movie('American Beauty', '2h','1999');
+$americanBeauty->setArray(['drama','thriller']);
 $murderOnTheOrientExpress = new Movie('Murder on the Orient Express', '2h', '2017', 'thriller');
 
+$newArray = $americanBeauty->getArray();
+
 echo $americanBeauty->getMovie();
+foreach ($newArray as $item) {
+    echo "<span>$item</span>";
+}
 echo '<br>';
 echo $murderOnTheOrientExpress->getMovie();
