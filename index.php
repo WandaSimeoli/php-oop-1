645ee49e85d7e17f17dfd1ed91cb5c27d7a29b41
WandaSimeoli <wandasimeoli@gmail.com>
2023-09-01 17:25:11 +0200

    Variabili e metodi

--- index.php
+++ index.php
@@ -4,9 +4,15 @@ class Movie {
     public $title;
     public $runningTime;
     public $year;
+
+    public function getNameYear() {
+        return $this->title.' '.$this->year;
+    }
 }
 
 $americanBeauty = new Movie();
-$americanBeauty->title = '';
-$americanBeauty->runningTime = '';
-$americanBeauty->year = '';
+$americanBeauty->title = 'American Beauty';
+$americanBeauty->runningTime = '2h';
+$americanBeauty->year = '1999';
+
+echo $americanBeauty->getNameYear();
