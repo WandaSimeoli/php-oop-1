014c44d41069255b6ac845e084f16bed60be2d29
WandaSimeoli <wandasimeoli@gmail.com>
2023-09-01 18:03:17 +0200

    Costructor

--- index.php
+++ index.php
@@ -5,14 +5,19 @@ class Movie {
     public $runningTime;
     public $year;
 
+    public function __construct($title, $runningTime, $year)
+    {
+       $this->title = $title;
+       $this->runningTime = $runningTime;
+       $this->year = $year;
+       
+    }
+
     public function getNameYear() {
         return $this->title.' '.$this->year;
     }
 }
 
-$americanBeauty = new Movie();
-$americanBeauty->title = 'American Beauty';
-$americanBeauty->runningTime = '2h';
-$americanBeauty->year = '1999';
+$americanBeauty = new Movie('American Beauty', '2h','1999');
 
 echo $americanBeauty->getNameYear();
