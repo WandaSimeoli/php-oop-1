937364c4680c6c37939d6d74168307dd4f2e69ee
WandaSimeoli <wandasimeoli@gmail.com>
2023-09-01 17:07:47 +0200

    Initialization

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+class Movie {
+    public $title;
+    public $runningTime;
+    public $year;
+}
+
+$americanBeauty = new Movie();
+$americanBeauty->title = '';
+$americanBeauty->runningTime = '';
+$americanBeauty->year = '';
